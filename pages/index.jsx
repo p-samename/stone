d9bd867dd71b33867b17601424976f0e47053f8f
dm0423 <dm0423@gmail.com>
2023-07-28 13:54:07 +0900

    pages > api 생성

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,24 @@
+import axios from 'axios';
 import { Layout, HeadOption } from '@components/index';
+import { useEffect, useState } from 'react';
+
 const TITLE = '메인';
 export default function Main(props) {
-  return <div>main</div>;
+  const [data, setData] = useState(null);
+  async function getData() {
+    try {
+      const { data } = await axios.get('http://localhost:3000/api/data');
+      setData(data.productsData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 Main.getLayout = function getLayout(page) {
