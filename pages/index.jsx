ccdfdf82dec4b1548860465ff342760e5cff17b0
dm0423 <dm0423@gmail.com>
2023-07-28 10:05:34 +0900

    배포 test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,7 @@
 import { Layout, HeadOption } from '@components/index';
 const TITLE = '메인';
 export default function Main(props) {
-  const { data } = props; // props에서 data를 추출
-  return <div>main {data.name}</div>;
+  return <div>main</div>;
 }
 
 Main.getLayout = function getLayout(page) {
@@ -12,11 +11,3 @@ Main.getLayout = function getLayout(page) {
     </Layout>
   );
 };
-
-// export const getServerSideProps = async (context) => {
-//   // Fetch data from external API
-//   const data = { name: 'dm' };
-
-//   // Pass data to the page via props
-//   return { props: { data } };
-// };
