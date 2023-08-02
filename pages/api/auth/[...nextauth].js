e273c4703341d1e3fb15a0f9d57025787880c162
dm0423 <dm0423@gmail.com>
2023-08-02 18:18:28 +0900

    환경변수 secret 추가

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import KakaoProvider from 'next-auth/providers/kakao';
 
 export default NextAuth({
+  secret: process.env.SECRET,
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID,
