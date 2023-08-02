e9ca395c1d5296f5a51116cf5e9823e38c39fb0e
dm0423 <dm0423@gmail.com>
2023-08-02 17:59:48 +0900

    카카오 로그인

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,4 +8,26 @@ export default NextAuth({
       clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
   ],
+  //   session: {
+  //     // 세션 유지 기간 설정 (1초)
+  //     maxAge: 1,
+  //   },
+
+  callbacks: {
+    //access 토큰 받아옴
+    // async jwt({ token, account, profile }) {
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //     token.id = profile.id;
+    //   }
+    //   return token;
+    // },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      session.user.id = token.sub;
+      // console.log("token", token);
+      return session;
+    },
+  },
 });
--- pages/kakao.jsx
+++ pages/kakao.jsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 
 export default function Kakao() {
   const { data: session } = useSession();
-
   useEffect(() => {
     console.log(session);
   }, [session]);
