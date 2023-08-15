a048453a3b3d86ea536b9f86abd25a93006f3166
stone <qkrehfaod@naver.com>
2023-08-15 18:46:22 +0900

    mainProductSlides line-clamp-3(3줄이상시에 ...처리 추가)

--- components/slide/MainProductSlideContents.jsx
+++ components/slide/MainProductSlideContents.jsx
@@ -67,7 +67,7 @@ export function MainProductSlideContents({ additionalClass, productType }) {
                 <div className="px-24px">
                   <div className="h-60 mb:h-[250px]" style={{ background: `url(${product.imgPath}) no-repeat center`, backgroundSize: 'contain' }} />
                   <p className="text-center mb-8px text-body2">{product.title}</p>
-                  <p className="text-center w-25 mx-auto text-body5">{product.description}</p>
+                  <p className="text-center w-25 mx-auto text-body5 w-[280px] overflow-hidden text-ellipsis break-words line-clamp-3">{product.description}</p>
                 </div>
               </SwiperSlide>
             );
