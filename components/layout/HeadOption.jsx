c24c8227c9351106d111ba5aaa87095a4614e294
stone <qkrehfaod@naver.com>
2023-08-22 23:49:58 +0900

    meta tag 변경

--- components/layout/HeadOption.jsx
+++ components/layout/HeadOption.jsx
@@ -13,12 +13,12 @@ export function HeadOption({ title, description, children }) {
       <meta key="viewport" name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
       <meta key="keywords" name="keywords" content="stone, portfolio, 포트폴리오, 박동명" />
       <meta key="description" name="description" content={_description} />
-      <meta key="og:type" property="og:type" content="website" />
-      <meta key="og:site_name" property="og:site_name" content="stone" />
-      <meta key="og:locale" property="og:locale" content="ko" />
-      <meta key="og:image" property="og:image" content="/images/products/contentsImg/1.png" />
-      <meta key="og:title" property="og:title" content={_title} />
-      <meta key="og:description" property="og:description" content={_description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="stone" />
+      <meta property="og:locale" content="ko" />
+      <meta property="og:image" content="/images/products/contentsImg/1.png" />
+      <meta property="og:title" content={_title} />
+      <meta property="og:description" content={_description} />
       <script async src="https://developers.kakao.com/sdk/js/kakao.js"></script>
       {children}
     </Head>
